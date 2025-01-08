0076c9e2c503d73d163c6b50b14410012c4796c1
Lucas Almeida <87536441+LucasSAlmeida@users.noreply.github.com>
2025-01-08 17:50:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import plotly.graph_objects as go
 from utils.DrawPitchSetPieces import create_pitch_plotly
 
 # Função para criar gráficos de ações no campo
-def plot_actions(df, title, action_type, selected_game):
+def plot_actions(df, title, action_type, selected_game, key):
     st.title(title)
     filtered_df = df[(df['evento'] == action_type) & (df['jogo'] == selected_game)]
     pitch_figure = create_pitch_plotly(80, 60, 'yards', 'black', df=filtered_df)
@@ -36,7 +36,7 @@ def plot_actions(df, title, action_type, selected_game):
             arrowwidth=2,
             arrowcolor='black'
         )
-    st.plotly_chart(pitch_figure)
+    st.plotly_chart(pitch_figure, key=key)
 
 # Carregar os dados
 df = pd.read_csv('https://raw.githubusercontent.com/LucasSAlmeida/dados/refs/heads/main/teste_ic.csv')
@@ -49,17 +49,16 @@ st.subheader("Quantidade de cruzamentos que são feitos para a grande área, o q
 selected_game = st.selectbox('Selecione um jogo', df.jogo.unique())
 
 # Gráfico 1: Cruzamentos na grande área
-plot_actions(df, 'Cruzamentos na grande área', 'Cruzamento', selected_game)
+plot_actions(df, 'Cruzamentos na grande área', 'Cruzamento', selected_game, key="crossings")
 
 # Gráfico 2: Finalizações e gols a partir de cruzamentos
 st.title("Finalizações e gols a partir de cruzamentos")
 df_shots = df[(df["evento"] == "Finalização") | (df["evento"] == "Gol")]
 selected_action = st.selectbox('Selecione uma ação', df_shots.evento.unique())
-plot_actions(df_shots, f'{selected_action}s a partir de cruzamentos', selected_action, selected_game)
+plot_actions(df_shots, f'{selected_action}s a partir de cruzamentos', selected_action, selected_game, key="shots")
 
 # Gráfico 3: Interceptações de cruzamentos
-plot_actions(df, 'Interceptações dos cruzamentos', 'Interceptação', selected_game)
-
+plot_actions(df, 'Interceptações dos cruzamentos', 'Interceptação', selected_game, key="interceptions")
 
 
 
